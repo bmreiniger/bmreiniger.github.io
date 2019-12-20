b129894539b5bea7bc530409331f71b13e0a8354
Ben Reiniger <bmreiniger@users.noreply.github.com>
2019-12-19 21:18:27 -0600

    More imports from academic site

---
+++ _includes/header+menu.js
@@ -0,0 +1,10 @@
+var menu = "  <ul>"
+menu += "  <li><a href='/academicSite'>Home</a></li>"
+menu += "  <li><a href='/academicSite/aboutme.html'>About Me</a></li>"
+menu += "  <li><a href='/'>Coding <span class='fas fa-external-link-alt'></span></a></li>"
+menu += "  <li><a href='/academicSite/research/index.html'>Research</a></li>"
+menu += "  <li><a href='/academicSite/teaching'>Teaching</a>"
+menu += "  <li><a href='/academicSite/links.html'>Links</a></li>"
+menu += "  </ul>"
+
+document.getElementById("navbar").innerHTML = menu;

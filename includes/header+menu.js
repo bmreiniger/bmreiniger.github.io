3bcf60896b2d930c44d9da43ae6e07ef8928a3e2
bmreiniger <40676050+bmreiniger@users.noreply.github.com>
2019-12-20 15:16:41 -0600

    Hide blog menu link while under construction
    (I know, better testing/branches, but the redesign was needed and my local jekyll is misbehaving)

--- includes/header+menu.js
+++ includes/header+menu.js
@@ -1,7 +1,7 @@
 var menu = "  <ul>"
 menu += "  <li><a href='/'>Home</a></li>"
 menu += "  <li><a href='/aboutme.html'>About Me</a></li>"
-menu += "  <li><a href='/blog'>Bloggish</a></li>"
+//menu += "  <li><a href='/blog'>Bloggish</a></li>"
 menu += "  <li><a href='/academicSite'>Academic archive</a></li>"
 menu += "  <li><a href='/links.html'>Links</a></li>"
 menu += "  </ul>"

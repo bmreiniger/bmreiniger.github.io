a5033d63eb1b57f79b965ba04e89ebbdcd570d2c
Ben Reiniger <bmreiniger@users.noreply.github.com>
2019-12-19 21:35:00 -0600

    Move blog, update menu

--- includes/header+menu.js
+++ includes/header+menu.js
@@ -1,10 +1,9 @@
 var menu = "  <ul>"
-menu += "  <li><a href='/academicSite'>Home</a></li>"
-menu += "  <li><a href='/academicSite/aboutme.html'>About Me</a></li>"
-menu += "  <li><a href='/'>Coding <span class='fas fa-external-link-alt'></span></a></li>"
-menu += "  <li><a href='/academicSite/research/index.html'>Research</a></li>"
-menu += "  <li><a href='/academicSite/teaching'>Teaching</a>"
-menu += "  <li><a href='/academicSite/links.html'>Links</a></li>"
+menu += "  <li><a href='/'>Home</a></li>"
+menu += "  <li><a href='/aboutme.html'>About Me</a></li>"
+menu += "  <li><a href='/blog'>Bloggish</a></li>"
+menu += "  <li><a href='/academicSite'>Academic archive</a></li>"
+menu += "  <li><a href='/links.html'>Links</a></li>"
 menu += "  </ul>"
 
 document.getElementById("navbar").innerHTML = menu;

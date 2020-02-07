9f1eb7b7ddd67596960b1782d30c17be24ba20dd
Ben Reiniger <bmreiniger@users.noreply.github.com>
2020-02-06 22:02:05 -0600

    Overhaul jekyll

--- includes/header+menu.js
+++ includes/header+menu.js
@@ -1,6 +1,7 @@
 var menu = "  <ul>"
 menu += "  <li><a href='/'>Home</a></li>"
 menu += "  <li><a href='/aboutme.html'>About Me</a></li>"
+menu += "  <li><a href='/blog/'>Blog</a></li>"
 menu += "  <li><a href='/academicSite'>Academic archive</a></li>"
 menu += "  <li><a href='/links.html'>Links</a></li>"
 menu += "  </ul>"
